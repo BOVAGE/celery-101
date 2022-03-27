415d04b51a6a6a95aa313ac8fc8f709c3c36b80a
bovage <fayemibolu2@gmail.com>
2022-03-27 11:08:45 -1200

    add auto import to init & debug task

--- core/celery.py
+++ core/celery.py
@@ -10,3 +10,7 @@ app = Celery('core')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
